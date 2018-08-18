f5a1c23ac915f5c33aaac5bbec63986a1f0c18a5
mateouagrm <cesarfuentes0594@gmail.com>
2018-08-18 15:52:42 -0400

    chat de facebook la parte lateral izquierdo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-10 ">
+</div>
+<div class="col-md-2 fondo-facebook">
+    <div class="row">
+        <span>juegos instantaneos</span><span class="pull-right">más</span>
+    </div>
+    <div class="row">
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <span>tus juegos</span><span class="pull-right">más</span>
+    </div>
+    <div class="row">
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-4 pl-0 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <span>tus Páginas</span>
+    </div>
+    <div class="row">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>practica css</p>
+        </div>
+        <div class="col-md-2 notificacion">
+            <p class="contador">9</p>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <span>Contactos</span>
+    </div>
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>Cesar Fuentes</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>flanklin salas</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>chapaco medrano</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>Doris :)</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>Christian martines</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+    <div class="row">
+        <span>MÁS CONTACTOS (4)</span>
+    </div>
+    <div class="row contacto">
+        <div class="col-md-3 pr-0">
+            <img src="{{asset('img/preg.png')}}" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-md-7 pl-0">
+            <p>yerson Fuentes</p>
+        </div>
+        <div class="col-md-2 point">
+        </div>
+    </div>
+
+
+</div>
+
+@endsection
